50c5bf48d8e4221f1afc2ed784498c0cf587e2e6
elisa-gc <139937145+elisa-gc@users.noreply.github.com>
2023-07-28 13:25:16 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '12e91c54-e4e1-473d-837f-de2479e6c160';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
